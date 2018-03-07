2e0dab5464dca389bb4e0aba04ec163de0a17b58
Ryan <Rwhytsell67@gmail.com>
2018-03-07 15:31:18 -0500

    Spaces in filename fix

--- src/renderer.js
+++ src/renderer.js
@@ -23,10 +23,10 @@ function readFolder(newFolder) {
                         else{
                             let id = `${newFolder}${file}/`;
                             if(stats.isDirectory()){
-                                document.getElementById('display-files').innerHTML += `<li id=${id} ondblclick="readFolder(this.id)"><i class="fa fa-folder-open"></i> ${file}</li>`;
+                                document.getElementById('display-files').innerHTML += `<li id="${id}" ondblclick="readFolder(this.id)"><i class="fa fa-folder-open"></i> ${file}</li>`;
                             }
                             else {
-                                document.getElementById('display-files').innerHTML += `<li id=${id} ondblclick="openFile(this.id)"><i class="fa fa-file"></i> ${file}</li>`;
+                                document.getElementById('display-files').innerHTML += `<li id="${id}" ondblclick="openFile(this.id)"><i class="fa fa-file"></i> ${file}</li>`;
                             }
                         }
                     });
