ba3957d90e7086810f15221a0f23d2a211b45b45
Ryan <Rwhytsell67@gmail.com>
2018-03-17 15:34:32 -0400

    Added some documentation

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Mule
 A file manager using electron
+
+## Running Mule
+
+After downloading the source, change your terminals working directory to the projects root folder. 
+
+Then:   ` electron . `
+
+Mule should then start
+
+Works on Windows and Linux
+
+## Future plans
+* Right click menu
+* Cut Copy Paste
+* Favorite folders list
+* Use an API running on a node server instead of directly accessing the file system
+* Create way to link file systems
--- src/renderer.js
+++ src/renderer.js
@@ -3,21 +3,21 @@ const {shell} = require('electron');
 const os = require('os')
 const path = require('path');
 
-var home = os.homedir();
+var home = os.homedir(); // Home directory will be on the API as well
 home = home.replace(/\\/g, '/') + '/';
 console.log(home);
 
 function readFolder(newFolder) {
     if(newFolder == 'init'){ newFolder = home;}
     console.log(newFolder);
-    fs.readdir(newFolder, (err, files) => {
+    fs.readdir(newFolder, (err, files) => {  // Switch to fetching from an API that will be ran off of each instance
         if (err) throw err;
         else{
             document.getElementById('back').setAttribute("ondblclick", `readFolder("${path.dirname(newFolder).split(path.sep).pop() + '/'}")`)
             document.getElementById('path').innerHTML = newFolder;
             document.getElementById('listed-files').innerHTML = `<ol id="display-files"></ol>`;
             for (let file of files){
-                if(file.charAt(0) != '.'){
+                if(file.charAt(0) != '.' && file.slice(0,6).toLowerCase() != 'ntuser'){
                     fs.stat(newFolder+file, (err, stats) => {
                         if(err) throw err;
                         else{
