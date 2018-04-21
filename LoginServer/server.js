3203e94501df4ab46053d1a00c9fdf1d13bf26fd
Ryan <Rwhytsell67@gmail.com>
2018-04-21 12:16:34 -0400

    Remove env option for port (temp)

--- LoginServer/server.js
+++ LoginServer/server.js
@@ -7,24 +7,39 @@ var dbConfig = configFile.db;
 
 const client = new Client(dbConfig);
 
-client.connect()
-
-client.query('SELECT NOW()', (err, res) => {
-    console.log(err, res);
-    client.end();
-})
-
 var app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080;
+var port = 8080;
 
 var router = express.Router();
 
-router.get('/', function(req, res) {
+router.get('/user' , function(req, res){
     
+
+router.post('/login', function(req, res) {
+    const text = 'INSERT INTO users(name, email) VALUES($1, $2, NOW(), $3) RETURNING *'
+    const values = ['EMAIL', 'PASS_HASH', 'USERS_NAME']
+
+    // Connect to db and insert new user
+    client.connect()
+    client.query(text, values)
+    .then(res => {
+        console.log(res.rows[0])
+    })
+    .catch(e => console.error(e.stack))
+    })
+    
+});
+
+router.get('/mule', function(req, res) {
+
+});
+
+router.post('/mule', function(req, res) {
+
 });
 
 app.use('/api', router)
