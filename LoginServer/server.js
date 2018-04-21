ec0e31d2d6e5e60666c06a42de9c08db7ad5cc2d
Ryan <Rwhytsell67@gmail.com>
2018-04-21 16:41:51 -0400

    Getting crazy

--- LoginServer/server.js
+++ LoginServer/server.js
@@ -24,21 +24,27 @@ var router = express.Router();
 app.post('/api/login', function(req, res) {
     console.log('Post on login');
     const data = req.body.data;
-    const text = 'INSERT INTO user(email, pass_hash, date_created, name) VALUES($1, $2, NOW(), $3) RETURNING *';     // valueFormat = ['EMAIL', 'PASS_HASH', 'USERS_NAME', 'NAME']
+    const text = 'INSERT INTO user(email, pass_hash, date_created, name) VALUES($1, $2, NOW(), $3) RETURNING *'; // valueFormat = ['EMAIL', 'PASS_HASH', 'NAME']
     bcrypt.hash(data.password, 10, function(err, hash) {
         if(!err){
             const values = [data.email, hash, data.name];
             console.log(values);
-            client.connect(); // Connect to db
-            client.query(text, values) // Insert new user
-            .then(dat => res.send(dat.rows[0]))
-            .then(client.end())
-            .catch(err => console.error(err.stack));
+            client.connect()
+                .then(() => {
+                    console.log('Client Connected')
+                    client.query(text, values)
+                        .then(dat => res.send(dat.rows[0]))
+                        .then(() => {
+                            client.end();
+                            console.log('Client Disconnected');
+                        })
+                        .catch(err => res.send(err.stack));
+                })
+                .catch(err => res.send(err.stack));
         } else {
             res.send('Problem hashing password');
         }
     });
-
 });
 
 // router.get('/mule', function(req, res) {});
